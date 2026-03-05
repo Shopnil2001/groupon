2c6fc327ef334364ac473b3a32ee5ddc7e9b9a95
unknown <shopnilabdullah2001@gmail.com>
2026-03-06 03:13:10 +0600

    import fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 'use client';
 
 import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import MissionVision from './components/MissionVision';
-import Capabilities from './components/Capabilities';
-import Products from './components/Products';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import About from './Components/About';
+import MissionVision from './Components/MissionVision';
+import Capabilities from './Components/Capabilities';
+import Products from './Components/Products';
 
-import Contact from './components/Contact';
-import QuoteModal from './components/QuoteModal';
+import Contact from './Components/Contact';
+import QuoteModal from './Components/QuoteModal';
+
 
 export default function Home() {
   const [quoteOpen, setQuoteOpen] = useState(false);
@@ -24,9 +25,9 @@ export default function Home() {
       <Products />
    
       <Contact />
-
+     <QuoteModal isOpen={quoteOpen} onClose={() => setQuoteOpen(false)}></QuoteModal>
       {/* Quote Modal */}
-      <QuoteModal isOpen={quoteOpen} onClose={() => setQuoteOpen(false)} ></QuoteModal>
+      {/* <QuoteModal isOpen={quoteOpen} onClose={() => setQuoteOpen(false)} ></QuoteModal> */}
     </div>
   );
 }
